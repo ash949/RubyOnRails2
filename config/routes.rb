425ecea2e609b8382a65d28d26054cff3bb4eb18
ash949 <hamzaashoor949@hotmail.com>
2017-10-25 11:43:51 +0300

    added index, about, contact page, with simple styling and bootstrap 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  # root 'static_pages#index'
+  root to: 'static_page#index'
+  
+  get 'static_page/index'
+
+  get 'static_page/about'
+
+  get 'static_page/contact'
+
+  
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
