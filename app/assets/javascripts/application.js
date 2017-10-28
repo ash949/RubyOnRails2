8db834ce4cce11eeadc3f9e6e73b5018b9eed416
ash949 <hamzaashoor949@hotmail.com>
2017-10-28 16:33:04 +0200

    added google maps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+
+
+var initMap;
+var map;
+var myCoordinates = {lat: 31.5208912, lng: 34.4421586};
+initMap = function(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myCoordinates,
+    zoom: 16
+  });
+};
+
+var changeCoordinates;
+changeCoordinates = function(newCoordinates){
+  map.setCenter( JSON.parse(newCoordinates) );
+};
+
+var getCurrentBranch;
+getCurrentBranch = function(){
+  return $('#map-carousel .item.active').index();
+}
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,9 @@ Rails.application.routes.draw do
   
   get 'static_pages/index'
 
-  get 'static_pages/', to: 'static_pages#index', as: 'static_index'
-
   get 'static_pages/about'
 
   get 'static_pages/contact'
-
-  
-  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
