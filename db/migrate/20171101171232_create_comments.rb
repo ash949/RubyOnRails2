7beb17a06a8e53580dc2a93309b0589c00841ccd
ash949 <hamzaashoor949@hotmail.com>
2017-11-02 16:10:03 +0200

    removed conflicting migrations

--- db/migrate/20171101171232_create_comments.rb
+++ db/migrate/20171101171232_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true
       t.text :body
       t.decimal :rating
-      t.references :product, index: true,  foreign_key: true
+      t.references :product, index: true
 
       t.timestamps
     end
--- db/migrate/20171101173902_remove_foriegn_key_constraint_in_comments.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForiegnKeyConstraintInComments < ActiveRecord::Migration[5.1]
-  def change
-    remove_foreign_key :comments, :product
-    remove_foreign_key :comments, :user
-  end
-end
