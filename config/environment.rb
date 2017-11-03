9e6be75efd0dc7d5bdf8fe78bc298cdbdac20ed7
ash949 <hamzaashoor949@hotmail.com>
2017-11-03 19:18:27 +0200

    new cofigurations for user mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: 'apikey',
+  password: ENV['SENDGRID'],
+  enable_starttls_auto: true
+}
+
 
