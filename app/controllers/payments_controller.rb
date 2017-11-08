43e2cb432fb23bf36d291190a86d20833eb7a5ba
ash949 <hamzaashoor949@hotmail.com>
2017-11-08 23:11:22 +0200

    fixed payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,12 +4,7 @@ class PaymentsController < ApplicationController
     
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
-      customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source  => params[:stripeToken]
-      )
       charge = Stripe::Charge.create(
-        customer: customer.id,
         amount: current_user.active_order.total_cost, # amount in cents, again
         currency: "usd",
         source: token,
