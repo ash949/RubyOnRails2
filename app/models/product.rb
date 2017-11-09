7a4c1b6a091116b361c9c853257fe719daabbbcd
ash949 <hamzaashoor949@hotmail.com>
2017-11-09 15:05:55 +0200

    configured Rials.logger in test/development/production ENV. + debugged price method of product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -32,11 +32,16 @@ class Product < ApplicationRecord
   end
 
   def price
+    logger.debug "Converting product.price_in_cents from cents into dollars format"
     cents = (self.price_in_cents % 100).to_s
+    logger.debug "Cents: #{cents}"
     if ( cents.to_s.length == 1 )
       cents = '0' + cents
+      logger.debug "Cents: #{cents} (supposed to be two digits)"
     end
     dollars = (self.price_in_cents / 100).to_s
+    logger.debug "Dollars except the remaining cents : #{dollars}"
+    logger.debug "To be returned: #{dollars}.#{cents}"
     return dollars + '.' + cents
   end
   
