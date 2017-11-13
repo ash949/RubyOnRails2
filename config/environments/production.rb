f2143e240b315a57d9f331c529923deae6ad0fc6
ash949 <hamzaashoor949@hotmail.com>
2017-11-13 18:43:59 +0200

    reset heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://rubyhamza.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://rubyhamza.herokuapp.com', /http:\/\/rubyhamza.herokuapp.*/,'https://rubyhamza.herokuapp.com', /https:\/\/rubyhamza.herokuapp.*/ ]
+  config.action_cable.url = 'wss://glacial-badlands-91009.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://glacial-badlands-91009.herokuapp.com', /http:\/\/glacial-badlands-91009.herokuapp.*/,'https://glacial-badlands-91009.herokuapp.com', /https:\/\/glacial-badlands-91009.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.app_generators.javascript_engine = :javascript
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = {:host => 'rubyhamza.herokuapp.com', :protocol => 'http'}
+  config.action_mailer.default_url_options = {:host => 'glacial-badlands-91009.herokuapp.com', :protocol => 'http'}
   config.cache_store = :dalli_store,
                       (ENV["MEMCACHIER_SERVERS"] || "").split(","),
                       {:username => ENV["MEMCACHIER_USERNAME"],
